13fe12f39f3869bb7ebd721a0cf5befc4f7fe205
Pedro Pires <ph_pires@poli.ufrj.br>
2024-06-04 22:49:15 -0300

    new bullet
    new bullet

--- ship.gd
+++ ship.gd
@@ -22,8 +22,6 @@ func _process(delta):
 		velocity.y += 1
 	if (Input.is_action_pressed("move_up")):
 		velocity.y -= 1
-	if (Input.is_action_pressed("shoot")):
-		print("SHOOT THA THING")
 	
 	if (velocity.length()):
 		velocity = velocity.normalized()*speed
